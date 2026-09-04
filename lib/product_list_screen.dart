05917d4b9ac900643f965a33be209308f6e0baa3
exploremaruf <cyberistmaruf@gmail.com>
2026-09-04 08:52:53 +0600

    UI: Add Product list

--- lib/product_list_screen.dart
+++ lib/product_list_screen.dart
@@ -11,10 +11,44 @@ class _product_screeenState extends State<product_list_screeen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green, title: Text('Home')),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Home'),
+      ),
       body: Padding(
         padding: EdgeInsets.all(10),
-        child: Column(children: [TextField()]),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjvZZgpqBkk-uOVa2BoYHYDsPS7ZPM3CEhcfqw_vNkOQ&s=10',
+                    ),
+                    title: Text('Banana'),
+                    trailing: Text(
+                      'price: 10\$',
+                      style: TextStyle(color: Colors.orange, fontSize: 16),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(height: 2, color: Colors.greenAccent);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.green,
       ),
     );
   }
